91ceee9c75c05612d1ae714fd7147c4d99a2a8c8
Vladimir Alyamkin <ufna@ufna.ru>
2019-03-22 23:33:43 +0300

    Separate .so and .sym package processes

--- Source/AppCenter/Tools/Linux_AppCenter.sh
+++ Source/AppCenter/Tools/Linux_AppCenter.sh
@@ -6,18 +6,61 @@ printUsageExit()
 {
     SCRIPT_NAME=`basename "$0"`
     SCRIPT_NAME="Tools/Build/$SCRIPT_NAME"
-    echo -e "ERROR: $1\n"																2>&1
-    echo "Usage: $SCRIPT_NAME [ packBreakpadSymbols armeabi-v7a|arm64-v8a ]"			2>&1
+    echo -e "ERROR: $1\n"																			2>&1
+    echo "Usage: $SCRIPT_NAME [ packSharedObjects|dumpBreakpadSymbols armeabi-v7a|arm64-v8a ]"		2>&1
     exit 1
 }
 
-packBreakpadSymbols()
+packSharedObjects()
 {
 	if [ -z $1 ]; then
         printUsageExit "Platform arcitecture not set"
     fi
 
 	ARCH=$2
+
+	# Prepare directory to store symbols
+	ANDROID_DIR="Intermediate/Android"
+	pushd $ANDROID_DIR
+
+	SO_DIR="libs"
+	SO_ZIP="libs.zip"
+	rm -rf $SO_DIR
+	mkdir -p $SO_DIR
+
+	echo "Dump the symbols using the Breakpad toolchain as described in the https://chromium.googlesource.com/breakpad/breakpad/+/master/README.ANDROID#93"
+
+	# There two paths we need to process
+	JNI_DIR=APK/jni/$ARCH
+	LIBS_DIR=APK/libs/$ARCH
+
+	find $JNI_DIR $LIBS_DIR -name '*.so' | while read so_file; do
+		echo "Processing file '$so_file'"
+
+		cp --verbose $so_file $SO_DIR/$(basename $so_file)
+	done
+
+	# cleanup if we have some junk
+	rm -f $SO_ZIP
+
+	# zip symbol files into archive
+	zip -r -v $SO_ZIP $SO_DIR
+
+	# ANDROID_DIR
+	popd
+}
+
+dumpBreakpadSymbols()
+{
+	if [ -z $1 ]; then
+        printUsageExit "Platform arcitecture not set"
+    fi
+
+	ARCH=$2
+	SO_DIR="libs"
+	SO_ZIP="libs.zip"
+	rm -rf $SO_DIR
+	mkdir -p $SO_DIR
 
 	# Prepare directory to store symbols
 	ANDROID_DIR="Intermediate/Android"
@@ -30,9 +73,8 @@ packBreakpadSymbols()
 
 	echo "Dump the symbols using the Breakpad toolchain as described in the https://chromium.googlesource.com/breakpad/breakpad/+/master/README.ANDROID#93"
 
-	# There two paths we need to process
-	JNI_DIR=APK/jni/$ARCH
-	LIBS_DIR=APK/libs/$ARCH
+	# unzip .so files from zip
+	unzip -u $SO_ZIP
 
 	# Create a symbols.zip file with the following structure:
 	# $ unzip -l symbols.zip 
@@ -48,7 +90,7 @@ packBreakpadSymbols()
 	#  --------                   -------
 	#     24935                   6 files
 	
-	find $JNI_DIR $LIBS_DIR -name '*.so' | while read so_file; do
+	find $SO_DIR -name '*.so' | while read so_file; do
 		echo "Processing file '$so_file'"
 
 		sym_file=$so_file'.sym'
@@ -85,7 +127,8 @@ else
 		fi
 	fi
 
-	if [ "$1" == "packBreakpadSymbols" ]; then packBreakpadSymbols $2
+	if [ "$1" == "packSharedObjects" ]; then packSharedObjects $2
+	elif [ "$1" == "dumpBreakpadSymbols" ]; then dumpBreakpadSymbols $2
     else printUsageExit "Unknown 1st parameter: $1"
     fi
 fi
