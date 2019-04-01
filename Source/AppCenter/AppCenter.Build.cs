5a067c4c6ba792bf6a7074558663347282bb576a
v.alyamkin <v.alyamkin@corp.mail.ru>
2019-04-01 20:50:51 +0300

    Fix plugin defines while secret is not set

--- Source/AppCenter/AppCenter.Build.cs
+++ Source/AppCenter/AppCenter.Build.cs
@@ -35,8 +35,6 @@ public class AppCenter : ModuleRules
         bool bEnableCrashes = false;
         bool bEnableDistribute = false;
         bool bEnablePush = false;
-        string AppSecretAndroid = "";
-        string AppSecretIOS = "";
 
         // Read from config
         ConfigHierarchy Ini = ConfigCache.ReadHierarchy(ConfigHierarchyType.Engine, Target.ProjectFile.Directory, Target.Platform);
@@ -46,54 +44,47 @@ public class AppCenter : ModuleRules
         Ini.GetBool(SettingsSection, "bEnableCrashes", out bEnableCrashes);
         Ini.GetBool(SettingsSection, "bEnableDistribute", out bEnableDistribute);
         Ini.GetBool(SettingsSection, "bEnablePush", out bEnablePush);
-        Ini.GetString(SettingsSection, "AppSecretAndroid", out AppSecretAndroid);
-        Ini.GetString(SettingsSection, "AppSecretIOS", out AppSecretIOS);
         bool bAnyModuleEnabled = (bEnableAnalytics | bEnableCrashes | bEnableDistribute | bEnablePush);
 
+        PublicDefinitions.Add("WITH_APPCENTER=" + (bAnyModuleEnabled ? "1" : "0"));
         PublicDefinitions.Add("WITH_APPCENTER_ANALYTICS=" + (bEnableAnalytics ? "1" : "0"));
         PublicDefinitions.Add("WITH_APPCENTER_CRASHES=" + (bEnableCrashes ? "1" : "0"));
         PublicDefinitions.Add("WITH_APPCENTER_DISTIBUTE=" + (bEnableDistribute ? "1" : "0"));
         PublicDefinitions.Add("WITH_APPCENTER_PUSH=" + (bEnablePush ? "1" : "0"));
 
-        if (Target.Platform == UnrealTargetPlatform.Android)
+        if(bAnyModuleEnabled)
         {
-            // @TODO https://github.com/PushkinStudio/AppCenter-SDK-UE4/issues/5
-            PublicDefinitions.Add("WITH_APPCENTER=1");
-
-            PrivateIncludePaths.Add("AppCenter/Private/Android");
-
-            PublicDependencyModuleNames.AddRange(new string[] { "Launch" });
-
-            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
-            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "AppCenter_UPL_Android.xml"));
-
-            /** 
-             * Application.mk
-             * 
-             * APP_STL := gnustl_static
-             * APP_ABI := armeabi-v7a, arm64-v8a
-             * APP_CXXFLAGS := -std=c++11 -D__STDC_LIMIT_MACROS
-             * APP_PLATFORM := android-19
-             */
-            string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "ThirdParty");
-            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "src"));
-
-            PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "armeabi-v7a"));
-            PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "arm64-v8a"));
-            PublicAdditionalLibraries.Add("breakpad_client");
-        }
-        else if (Target.Platform == UnrealTargetPlatform.IOS)
-        {
-            bool bEnableAppCenter = bAnyModuleEnabled && (AppSecretIOS != "");
-            PublicDefinitions.Add("WITH_APPCENTER=" + (bEnableAppCenter ? "1" : "0"));
-
-            PrivateIncludePaths.Add("AppCenter/Private/IOS");
-
-            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
-            AdditionalPropertiesForReceipt.Add("IOSPlugin", Path.Combine(PluginPath, "AppCenter_UPL_IOS.xml"));
-
-            if (bEnableAppCenter)
+            if (Target.Platform == UnrealTargetPlatform.Android)
             {
+                PrivateIncludePaths.Add("AppCenter/Private/Android");
+
+                PublicDependencyModuleNames.AddRange(new string[] { "Launch" });
+
+                string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+                AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "AppCenter_UPL_Android.xml"));
+
+                /** 
+                 * Application.mk
+                 * 
+                 * APP_STL := gnustl_static
+                 * APP_ABI := armeabi-v7a, arm64-v8a
+                 * APP_CXXFLAGS := -std=c++11 -D__STDC_LIMIT_MACROS
+                 * APP_PLATFORM := android-19
+                 */
+                string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "ThirdParty");
+                PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "src"));
+
+                PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "armeabi-v7a"));
+                PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "arm64-v8a"));
+                PublicAdditionalLibraries.Add("breakpad_client");
+            }
+            else if (Target.Platform == UnrealTargetPlatform.IOS)
+            {
+                PrivateIncludePaths.Add("AppCenter/Private/IOS");
+
+                string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+                AdditionalPropertiesForReceipt.Add("IOSPlugin", Path.Combine(PluginPath, "AppCenter_UPL_IOS.xml"));
+
                 // The AppCenter.framework is required to start the SDK. If it is not added to the project, 
                 // the other modules won't work and your app won't compile.
                 PublicAdditionalFrameworks.Add(
@@ -144,10 +135,6 @@ public class AppCenter : ModuleRules
                     );
                 }
             }
-        }
-        else
-        {
-            PublicDefinitions.Add("WITH_APPCENTER=0");
         }
     }
 }
