f8961ba5ca3002d0d6d10a7367483d7b86566665
Vladimir Alyamkin <ufna@ufna.ru>
2019-04-22 17:47:41 +0300

    Minor build script update

--- Source/AppCenter/AppCenter.Build.cs
+++ Source/AppCenter/AppCenter.Build.cs
@@ -14,8 +14,8 @@ public class AppCenter : ModuleRules
             new string[]
             {
                 "Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
+                // ... add other public dependencies that you statically link with here ...
+            }
             );
 
 
@@ -26,9 +26,19 @@ public class AppCenter : ModuleRules
                 "Engine",
                 "Slate",
                 "SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
+                // ... add private dependencies that you statically link with here ...	
+            }
             );
+
+        // Setup platform include paths
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PrivateIncludePaths.Add("AppCenter/Private/Android");
+        }
+        else if (Target.Platform == UnrealTargetPlatform.IOS)
+        {
+            PrivateIncludePaths.Add("AppCenter/Private/IOS");
+        }
 
         // Configure build defines
         bool bAppCenterEnabled = false;
@@ -57,33 +67,32 @@ public class AppCenter : ModuleRules
             {
                 bAppCenterEnabled = true;
 
-                PrivateIncludePaths.Add("AppCenter/Private/Android");
-
                 PublicDependencyModuleNames.AddRange(new string[] { "Launch" });
 
                 string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
                 AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "AppCenter_UPL_Android.xml"));
 
-                /** 
-                 * Application.mk
-                 * 
-                 * APP_STL := gnustl_static
-                 * APP_ABI := armeabi-v7a, arm64-v8a
-                 * APP_CXXFLAGS := -std=c++11 -D__STDC_LIMIT_MACROS
-                 * APP_PLATFORM := android-19
-                 */
-                string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "ThirdParty");
-                PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "src"));
+                if (bEnableCrashes)
+                {
+                    /** 
+                     * Application.mk
+                     * 
+                     * APP_STL := gnustl_static
+                     * APP_ABI := armeabi-v7a, arm64-v8a
+                     * APP_CXXFLAGS := -std=c++11 -D__STDC_LIMIT_MACROS
+                     * APP_PLATFORM := android-19
+                     */
+                    string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "ThirdParty");
+                    PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "src"));
 
-                PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "armeabi-v7a"));
-                PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "arm64-v8a"));
-                PublicAdditionalLibraries.Add("breakpad_client");
+                    PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "armeabi-v7a"));
+                    PublicLibraryPaths.Add(Path.Combine(ThirdPartyPath, "Breakpad", "lib", "arm64-v8a"));
+                    PublicAdditionalLibraries.Add("breakpad_client");
+                }
             }
             else if (Target.Platform == UnrealTargetPlatform.IOS && AppSecretIOS != "")
             {
                 bAppCenterEnabled = true;
-
-                PrivateIncludePaths.Add("AppCenter/Private/IOS");
 
                 string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
                 AdditionalPropertiesForReceipt.Add("IOSPlugin", Path.Combine(PluginPath, "AppCenter_UPL_IOS.xml"));
